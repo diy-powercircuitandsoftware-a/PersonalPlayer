6152d2abe8d62b41c477c11fee996e3f4b02dbe7
Annop Chawwalitsitthikun <sourcecode@annopnod.com>
2021-10-06 19:33:20 +0700

    start project
    start project

---
+++ Compiler/src/Parser/lex.cpp
@@ -0,0 +1,16 @@
+#include "lex.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+lex::lex()
+{
+    //ctor
+}
+
+lex::~lex()
+{
+    //dtor
+}
+
---
+++ Compiler/src/Parser/lex.h
@@ -0,0 +1,16 @@
+#ifndef LEX_H
+#define LEX_H
+
+
+class lex
+{
+    public:
+        lex();
+        virtual ~lex();
+
+    protected:
+
+    private:
+};
+
+#endif // LEX_H
